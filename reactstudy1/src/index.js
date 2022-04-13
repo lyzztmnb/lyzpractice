67b4475d9a1f3fb04eb35174d432596ad680c72d
liuyunzhao <1156240356@qq.com>
2022-04-13 11:23:11 +0800

    add react practice1

---
+++ reactstudy1/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <div className='bigbox'>
+    <h3>react练习1、hello word</h3>
+    <h2>hellow word</h2>
+  </div>,
+  document.getElementById('root')
+)
+
+reportWebVitals();
